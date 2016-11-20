f01f42099aac2c7b00066959b21744e4ab9f2165
Jose Witjaksono <hpsx80@gmail.com>
2016-11-20 19:05:03 +0700

    Penambahan Fitur Upload & Download

--- app/src/main/java/id/sch/smktelkom_mlg/project/xirpl607162534/mokletblitz/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xirpl607162534/mokletblitz/MainActivity.java
@@ -1,13 +1,188 @@
 package id.sch.smktelkom_mlg.project.xirpl607162534.mokletblitz;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Base64;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import id.sch.smktelkom_mlg.project.xirpl607162534.mokletblitz.app.AppController;
+import id.sch.smktelkom_mlg.project.xirpl607162534.mokletblitz.view.activity.ViewActivity;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-	}
+    public static final int REQUEST_CODE = 123;
+
+    Button buttonChoose;
+    FloatingActionButton buttonUpload;
+    Toolbar toolbar;
+    ImageView imageView;
+    EditText txt_name;
+    Bitmap bitmap;
+    int success;
+    int PICK_IMAGE_REQUEST = 1;
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    private String UPLOAD_URL ="http://20.20.20.124/android/upload_image/upload.php";
+
+    private static final String TAG_SUCCESS = "success";
+    private static final String TAG_MESSAGE = "message";
+    private String KEY_IMAGE = "image";
+    private String KEY_NAME = "name";
+
+    String tag_json_obj = "json_obj_req";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        buttonChoose = (Button) findViewById(R.id.buttonChoose);
+        buttonUpload = (FloatingActionButton) findViewById(R.id.buttonUpload);
+
+        findViewById(R.id.button1).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivityForResult(new Intent(MainActivity.this, ViewActivity.class), REQUEST_CODE);
+            }
+        });
+
+        txt_name = (EditText) findViewById(R.id.editText);
+
+        imageView  = (ImageView) findViewById(R.id.imageView);
+
+        buttonChoose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showFileChooser();
+            }
+        });
+
+        buttonUpload.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                uploadImage();
+            }
+        });
+    }
+
+    public String getStringImage(Bitmap bmp){
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bmp.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+        byte[] imageBytes = baos.toByteArray();
+        String encodedImage = Base64.encodeToString(imageBytes, Base64.DEFAULT);
+        return encodedImage;
+    }
+
+    private void uploadImage(){
+        final ProgressDialog loading = ProgressDialog.show(this, "Uploading...", "Please wait...", false, false);
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, UPLOAD_URL,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Log.d(TAG, "Response: " + response.toString());
+
+                        try {
+                            JSONObject jObj = new JSONObject(response);
+                            success = jObj.getInt(TAG_SUCCESS);
+
+                            if (success == 1) {
+                                Log.d("v Add", jObj.toString());
+
+                                Toast.makeText(MainActivity.this, jObj.getString(TAG_MESSAGE), Toast.LENGTH_LONG).show();
+
+                                kosong();
+
+                            } else {
+                                Toast.makeText(MainActivity.this, jObj.getString(TAG_MESSAGE), Toast.LENGTH_LONG).show();
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                        loading.dismiss();
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        loading.dismiss();
+
+                        Toast.makeText(MainActivity.this, error.getMessage().toString(), Toast.LENGTH_LONG).show();
+                        Log.d(TAG, error.getMessage().toString());
+                    }
+                }){
+            @Override
+            protected Map<String, String> getParams() {
+
+                Map<String,String> params = new HashMap<String, String>();
+
+
+                params.put(KEY_IMAGE, getStringImage(bitmap));
+                params.put(KEY_NAME, txt_name.getText().toString().trim());
+
+
+                Log.d(TAG, ""+params);
+                return params;
+            }
+        };
+
+        AppController.getInstance().addToRequestQueue(stringRequest, tag_json_obj);
+    }
+
+    private void showFileChooser() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            Uri filePath = data.getData();
+            try {
+
+                bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), filePath);
+
+                imageView.setImageBitmap(bitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void kosong(){
+        imageView.setImageResource(0);
+        txt_name.setText(null);
+    }
+
 }
